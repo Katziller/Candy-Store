93ad94eaec935f9bd28816d1f519e2e5c6f40a9a
Zen <katzziller@gmail.com>
2021-11-27 20:53:01 +0200

    Candy Commit

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import java.io.IOException;
@@ -26,16 +27,34 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
+        EditText text = findViewById (R.id.url);
+
+        text.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText (getApplicationContext(), "", Toast.LENGTH_SHORT ).show ();
+
+            }
+        } );
+
         Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
         ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
         Picasso.get().
                 load(uri).
                 into(candyStoreImageView);
 
+        Intent mapIntent = getIntent ();
+
+        if (mapIntent.resolveActivity(getPackageManager()) != null); {
+            startActivity(mapIntent);
+
+        }
+
 
     }
 
     public void createMapIntent(View view) {
+
         Uri url = Uri.parse ( "geo:0,0?q=618 E South St Orlando, FL 32801" );
         Intent intent = new Intent (Intent.ACTION_VIEW, url);
         intent.setPackage ("com.google.android.apps.maps");
