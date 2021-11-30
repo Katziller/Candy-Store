2d188f49850f94c782f02835c650cb506d9c5b5c
Zen <katzziller@gmail.com>
2021-11-30 05:07:01 +0200

    Candy Commit

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -31,22 +31,29 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
+    // ***
+    // TODO - Task 2 - Launch the Google Maps Activity
+    // ***
+
     public void createMapIntent(View view) {
 
-        Uri url = Uri.parse ( "geo:0,0?q=618 E South St Orlando, FL 32801" );
-        Intent mapIntent = new Intent (Intent.ACTION_VIEW, url);
+        Uri uriAdress = Uri.parse ( "geo:0,0?q=618 E South St Orlando, FL 32801" );
+        Intent mapIntent = new Intent (Intent.ACTION_VIEW, uriAdress);
         mapIntent.setPackage ("com.google.android.apps.maps");
+
         if (mapIntent.resolveActivity ( getPackageManager ()) != null) {
             startActivity ( mapIntent );
         }
 
     }
     // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
-
-    // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view) {
+
+        Uri uriAdress = Uri.parse ( "tel:0123456789" );
+        Intent dialIntent = new Intent (Intent.ACTION_DIAL);
+        startActivity (dialIntent);
+    }
 
 }
